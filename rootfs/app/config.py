0ed18f9f192b62ef66919f7bbf22bc82fb70ec78
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-09-27 22:07:53 +0200

    upd

--- rootfs/app/config.py
+++ rootfs/app/config.py
@@ -1,3 +1,5 @@
+""" Configuration parameters for pipeline
+"""
 import os.path
 
 DATA_DIR = '/data'
@@ -7,17 +9,17 @@ TIME_DOMAIN_DIR = os.path.join(DATA_DIR, 'time_domain')
 
 # path to data for canonical strategy
 CAN_PATHS = [os.path.join(DATA_DIR, 'vanilla_lightcurves.txt'),
-            os.path.join(DATA_DIR, 'vanilla_labels.txt')]
+             os.path.join(DATA_DIR, 'vanilla_labels.txt')]
 
 # path to raw data
 RAW_DIR = os.path.join(DATA_DIR, 'SIMGEN_PUBLIC_DES')
 
 # tags for supernova classes
 SN_IBC = ['1', '5', '6', '7', '8', '9', '10', '11', '13', '14', '16', '18',
-         '22', '23', '29', '45', '28']
+          '22', '23', '29', '45', '28']
 SN_II = ['2', '3', '4', '12', '15', '17', '19', '20', '21', '24', '25', '26',
-        '27', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
-        '41', '42', '43', '44']
+         '27', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
+         '40', '41', '42', '43', '44']
 
 # DES filters
 FILTERS = ['g', 'r', 'i', 'z']
@@ -33,6 +35,8 @@ TRAIN_LABEL = []
 
 # results
 OUT_DIR = os.path.join(DATA_DIR, 'results')
-DIAG_NAME = os.path.join(OUT_DIR, 'diag_batch_' + MODE + '_bazinTD_batch1.dat')
-QUERIES_NAME = os.path.join(OUT_DIR, 'queries_batch_' + MODE +
-                            '_bazinTD_batch1.csv')
+_DIAG_FNAME = 'diag_batch_' + MODE + '_bazinTD_batch1.dat'
+DIAG_NAME = os.path.join(OUT_DIR, _DIAG_FNAME)
+
+_QUERIES_FNAME = 'queries_batch_' + MODE + '_bazinTD_batch1.csv'
+QUERIES_NAME = os.path.join(OUT_DIR, _QUERIES_FNAME)
